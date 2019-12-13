1767ab6a80f81484d3deb80bd98b2aa4ab1e2122
Vagner Santos <santos.vagnerferreira@gmail.com>
2019-12-13 16:27:33 -0300

    setup jest tests and update readme

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import App from './App';
 
-ReactDOM.render(<div>Origin Frontend Take Home</div>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
