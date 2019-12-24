383aaa98e037187cb5781d8bca7358bf16aefb17
cintiafumi <cintiafumi@gmail.com>
2019-12-24 12:00:23 -0300

    access control allow origin

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL
-  // headers: {
-  //   'Access-Control-Allow-Origin': '*',
-  //   crossdomain: true
-  // }
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    crossdomain: true
+  }
 });
 
 export default api;
