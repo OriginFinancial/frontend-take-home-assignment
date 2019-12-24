eaa7cc30994d7249ff3a9676978802469a389c35
cintiafumi <cintiafumi@gmail.com>
2019-12-24 12:29:32 -0300

    gh-pages

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,6 +4,9 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   headers: {
     'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers':
+      'Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With',
+    'Access-Control-Allow-Methods': 'GET, PUT, POST',
     crossdomain: true
   }
 });
