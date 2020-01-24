7f31a0dd42f4697ecab288c9f5a29e4ec1ce4590
valtermartins1301 <valtermartins.1301@gmail.com>
2020-01-23 23:44:31 -0300

    feat(component) create Nav component

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Nav = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 2em 0 1em;
+`;
+
+Nav.displayName = 'Nav';
+
+export { Nav };
---
+++ src/components/Nav/NavItem.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const NavItem = styled.div`
+  padding: 1em;
+`;
+
+export { NavItem };
