2e6b096422ba45eb2d868dadfb1c67fe8ad7df31
valtermartins1301 <valtermartins.1301@gmail.com>
2020-01-23 23:47:49 -0300

    fix(component) use Plan component

--- src/components/Goal/Goal.js
+++ src/components/Goal/Goal.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import { navigate } from '@reach/router';
+import { object } from 'prop-types';
 import { Grid } from '../Layout';
 import { Typography } from '../Typography';
 import { Card, CardMedia, CardAction } from '../Card';
 import { Button } from '../Button';
-import { object } from 'prop-types';
+import * as Icons from '../Icons';
+import { Plan } from '../Plan';
 
 const propTypes = {
   goal: object.isRequired
 };
 
-const Goal = ({ goal }) => {
-  const openGoal = () => navigate(`details/${goal.id}`);
+const Goal = ({ goal: { plan, category, id, title } }) => {
+  const openGoal = () => navigate(`details/${id}`);
+  const Icon = Icons[category];
 
   return (
     <Grid item>
-      <Card data-testid={goal.title}>
-        <CardMedia>{goal.icon}</CardMedia>
-        <Typography variant="h4">{goal.title}</Typography>
-        <CardAction>
-          <Button onClick={openGoal}>Start setup</Button>
-        </CardAction>
+      <Card data-testid={title}>
+        <CardMedia>
+          <Icon />
+        </CardMedia>
+        <Typography variant="h4">{title}</Typography>
+
+        {plan ? (
+          <Plan amount={plan.amount} reachDate={plan.reachDate} />
+        ) : (
+          <CardAction>
+            <Button onClick={openGoal}>Start setup</Button>
+          </CardAction>
+        )}
       </Card>
     </Grid>
   );
