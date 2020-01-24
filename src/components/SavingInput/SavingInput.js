a1aa224cb960a1edb0abec682d173c3c18b8bff9
valtermartins1301 <valtermartins.1301@gmail.com>
2020-01-23 23:46:59 -0300

    feat(component) create SavingInput component

---
+++ src/components/SavingInput/SavingInput.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Styled from 'styled-components';
+import { func } from 'prop-types';
+import { Typography } from '../Typography';
+import { InputMonth, InputAmount } from '../Input';
+import { mq } from '../utils/mediaQueries';
+
+const propTypes = {
+  setAmount: func.isRequired,
+  setReach: func.isRequired
+};
+
+const Wrapper = Styled.div`
+  display: flex;
+  padding: 1em 0;
+  ${mq({
+    flexDirection: ['column', 'column', 'column', 'column', 'row']
+  })}
+`;
+
+const Amount = Styled.div`
+  ${mq({
+    padding: ['1em 0', '1em 0', '1em 0', '1em 0', '0 1em']
+  })}
+`;
+
+const Reach = Styled.div`
+  ${mq({
+    padding: ['1em 0', '1em 0', '1em 0', '1em 0', '0 1em']
+  })}
+`;
+
+const SavingInput = ({ setAmount, setReach }) => {
+  return (
+    <Wrapper>
+      <Amount>
+        <Typography variant="title2">Total amount</Typography>
+        <InputAmount setAmount={setAmount} />
+      </Amount>
+      <Reach>
+        <Typography variant="title2">Reach goal by</Typography>
+        <InputMonth type="month" setReach={setReach} />
+      </Reach>
+    </Wrapper>
+  );
+};
+
+SavingInput.propTypes = propTypes;
+
+export { SavingInput };
---
+++ src/components/SavingInput/index.js
@@ -0,0 +1,1 @@
+export { SavingInput } from './SavingInput';
