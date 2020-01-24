f3cd076f38104f8e5083ea6eaa50549d73a964df
valtermartins1301 <valtermartins.1301@gmail.com>
2020-01-23 23:50:21 -0300

    feat(page) update Details page

--- src/pages/Details/Details.js
+++ src/pages/Details/Details.js
@@ -1,31 +1,73 @@
-import React from 'react';
-import { string } from 'prop-types';
+import React, { useState } from 'react';
+import { string, func, array } from 'prop-types';
 import { Container } from '../../components/Layout';
+import dayjs from 'dayjs';
 import { Card, CardMedia, CardAction } from '../../components/Card';
 import { Typography } from '../../components/Typography';
 import { Button } from '../../components/Button';
-import goalsEnum from '../../enums/goals';
+import { Arrow } from '../../components/Icons';
+import { Nav, NavItem } from '../../components/Nav';
+import { Calculator } from '../../components/Calculator';
+import { SavingInput } from '../../components/SavingInput';
+import * as Icons from '../../components/Icons';
 
 const propTypes = {
-  goalId: string.isRequired
+  goals: array.isRequired,
+  goalId: string.isRequired,
+  updateGoal: func.isRequired,
+  navigate: func.isRequired
 };
 
-const Details = props => {
-  const goal = goalsEnum.find(goal => goal.id === props.goalId);
+const Details = ({ goals, goalId, updateGoal, navigate }) => {
+  const goal = goals.find(goal => goal.id === goalId);
+  const [amount, setAmount] = useState(0);
+  const [reachDate, setReach] = useState(dayjs());
+  const Icon = Icons[goal.category];
+
+  const saveGoal = () => {
+    const plan = { amount, reachDate };
+
+    updateGoal({
+      ...goal,
+      plan
+    });
+
+    navigate('/');
+  };
 
   return (
-    <Container maxWidth="sm">
-      <Typography variant="h3">{`Let's plan your saving goal.`}</Typography>
+    <Container maxWidth="md">
+      <Nav>
+        <NavItem>
+          <Button rounded onClick={() => navigate('/')}>
+            <Arrow size="xs" />
+          </Button>
+        </NavItem>
+        <NavItem>
+          <Typography variant="title1" color="blue">
+            {`Let's`} plan your <strong>saving goal</strong>.
+          </Typography>
+        </NavItem>
+      </Nav>
 
       {goal && (
-        <Card alignItems="left">
+        <Card alignItems="left" padding="2.5em">
           <CardMedia alignItems="left">
-            {goal.icon}
+            <Icon />
             <Typography variant="h2">{goal.title}</Typography>
-            <Typography variant="subtitle2">Saving goal</Typography>
+            <Typography variant="subtitle1" color="grey">
+              Saving goal
+            </Typography>
           </CardMedia>
+
+          <SavingInput setAmount={setAmount} setReach={setReach} />
+
+          <Calculator amount={amount} reachDate={reachDate} />
+
           <CardAction>
-            <Button minWidth="300px">Finish</Button>
+            <Button minWidth="lg" onClick={saveGoal}>
+              Finish
+            </Button>
           </CardAction>
         </Card>
       )}
--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,1 +1,19 @@
-export { Details } from './Details';
+import { Details } from './Details';
+import { connect } from 'react-redux';
+import { updateGoal, GOAL_STATE_KEY } from '../../store/goal';
+
+const mapStateToProps = state => {
+  return {
+    goals: state[GOAL_STATE_KEY].goals
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  updateGoal(goal) {
+    dispatch(updateGoal(goal));
+  }
+});
+
+const GoalDetails = connect(mapStateToProps, mapDispatchToProps)(Details);
+
+export default GoalDetails;
