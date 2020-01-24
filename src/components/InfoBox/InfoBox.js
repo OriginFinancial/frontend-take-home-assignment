dadfc79067dbc4078e5f8e3b2cb914bd2e11ddaf
valtermartins1301 <valtermartins.1301@gmail.com>
2020-01-23 23:41:11 -0300

    feat(component) create infoBox component

---
+++ src/components/InfoBox/InfoBox.js
@@ -0,0 +1,12 @@
+import Styled from 'styled-components';
+
+const InfoBox = Styled.div`
+  border: 1px solid #e1e8ed;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  width: 100%;
+`;
+
+export { InfoBox };
