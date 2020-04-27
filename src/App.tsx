c92dec7920e6ade940e9a7bd2d46700c039c1276
Gabriel Kozlowaski <gkm.kozlowaski@gmail.com>
2020-04-27 18:38:48 -0300

    Implement App with the home screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,66 @@
 import * as React from 'react';
+import styled from 'styled-components';
 import Header from './components/Header';
 import { GlobalStyle } from './styles/global';
+import { media } from './styles/mediaQueries';
+import homeSVG from './icons/home.svg';
+import SavingCard from './components/SavingCard';
+
+const StyledPageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: #f4f8fa;
+  min-height: 100vh;
+`;
+
+const StyledTitle = styled.h1`
+  color: #1b31a8;
+  font-size: 1.8rem;
+  line-height: 1.44;
+  margin-top: 2.3rem;
+  margin-bottom: 2.4rem;
+
+  ${media.desktop} {
+    font-size: 2.4rem;
+    line-height: 1.33;
+    margin-top: 5.4rem;
+    margin-bottom: 4rem;
+  }
+
+  & > strong {
+    font-weight: 600;
+  }
+`;
+
+const StyledSavingCard = styled(SavingCard)`
+  align-self: stretch;
+
+  ${media.mobile} {
+    flex-grow: 1;
+  }
+
+  ${media.desktop} {
+    align-self: center;
+    width: 56rem;
+  }
+`;
 
 const App: React.FunctionComponent = () => {
   return (
     <>
       <GlobalStyle />
-      <Header />
+      <StyledPageContainer>
+        <Header />
+        <StyledTitle>
+          Let&apos;s plan your <strong>saving goal</strong>
+        </StyledTitle>
+        <StyledSavingCard
+          iconSrc={homeSVG}
+          iconAlt="Little house with dollar sign on the bottom left"
+          title="Buy a house"
+        />
+      </StyledPageContainer>
     </>
   );
 };
