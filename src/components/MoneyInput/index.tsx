86039f7ef28c3ec93e368da21a4edb1924916af0
Gabriel Kozlowaski <gkm.kozlowaski@gmail.com>
2020-04-27 10:36:06 -0300

    Implement MoneyInput component

---
+++ src/components/MoneyInput/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledContainer = styled.div`
+  display: flex;
+`;
+
+const StyledLabel = styled.span`
+  padding: 1.6rem 2rem;
+  color: #657786;
+  font-size: 2.2rem;
+  line-height: 1.18;
+  letter-spacing: -0.018rem;
+  background: #f4f8fa;
+  border: 0.1rem solid #e1e8ed;
+  border-radius: 0.4rem 0 0 0.4rem;
+`;
+
+const StyledInput = styled.input`
+  width: 100%;
+  border: 0.1rem solid #e1e8ed;
+  border-left: none;
+  border-radius: 0 0.4rem 0.4rem 0;
+  font-size: 2rem;
+  line-height: 1.2rem;
+  font-weight: 600;
+  padding: 1.6rem;
+  letter-spacing: -0.016rem;
+`;
+
+interface Props {
+  className?: string;
+  onChange?: (e: React.FormEvent<HTMLInputElement>) => void;
+}
+
+const MoneyInput: React.FC<Props> = ({ className, onChange }) => (
+  <StyledContainer className={className}>
+    <StyledLabel>$</StyledLabel>
+    <StyledInput type="number" onChange={onChange} />
+  </StyledContainer>
+);
+
+export default MoneyInput;
