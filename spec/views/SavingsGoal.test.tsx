6aaabc7db2984fceaae64c1b7623374060bb01cf
Nathan Barreto <nathanmrtns@gmail.com>
2020-08-10 16:15:30 -0300

    Add test for savings page

--- spec/views/SavingsGoal.test.tsx
+++ spec/views/SavingsGoal.test.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import SavingsGoal from '../../src/views/SavingGoal';
+import CurrencyField from '../../src/components/CurrencyField';
+import DateInput from '../../src/components/DateInput';
 
 describe('Savings Goal Page', () => {
   it('renders savings container', () => {
-    const component = shallow(<SavingsGoal />);
+    const component = mount(<SavingsGoal />);
     expect(component.find("ContainerTitle").text()).toEqual('Buy a house');
-    expect(component.find("ConfirmButton").text()).toEqual('Confirm');
+    expect(component.find("ConfirmButton").exists()).toEqual(true);
+    expect(component.find(CurrencyField).exists()).toEqual(true);
+    expect(component.find(DateInput).exists()).toEqual(true);
   });
 });
